4c80b9f764d99c4f06f1ca267953d82248302363
milenabotelho <milenaschafflen@gmail.com>
2020-03-05 15:40:22 -0300

    Projeto Complementado

--- src/Curso/Git.java
+++ src/Curso/Git.java
@@ -1,10 +1,25 @@
 package Curso;
 
+import java.util.Scanner;
+
 public class Git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+Scanner sc = new Scanner(System.in);
+		
+		int numero;
+		 System.out.println("Informe um número: ");
+		 numero = sc.nextInt();
+		 
+		 if(numero % 2 == 0) {
+			 System.out.println("O numero é par : "+numero);
+		 }else {
+			 System.out.println("O numero é impar : "+numero);
+			 
+		 }
+		
+		
+		sc.close();
 	}
 
 }
